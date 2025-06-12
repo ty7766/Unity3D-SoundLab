6f1c4c3f64b10aed63c708ee2cbb3312b9bb5455
hyojoon Lim <hyojoon42@naver.com>
2025-06-12 22:27:40 +0900

    feature : SpawnManager 추가

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+using UnityEditor.VisionOS;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;   //장애물
+
+    private Vector3 spawnPos = new Vector3(25, 0, 0); //초기 생성 위치
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    void Start()
+    {
+        //장애물 초기 위치에 생성
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
