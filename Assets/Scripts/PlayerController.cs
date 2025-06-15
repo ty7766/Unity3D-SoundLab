349e37595ae88fe09d66a4e09ceb93429ade7afb
hyojoon Lim <hyojoon42@naver.com>
2025-06-15 12:36:49 +0900

    feature : 이펙트 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,11 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;       //중력 조절
     public bool isOnGround = true;      //2단 점프 방지용
     public bool gameOver = false;       //게임 오버 판정
+    public ParticleSystem explosionParticle;       //폭발 이펙트
+    public ParticleSystem dirtParticle;            //먼지 이펙트
 
     private Rigidbody playerRb;
     private Animator playerAnim;
-
 
     //------------- 초기화 ---------------
     void Start()
@@ -28,22 +29,28 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
     }
 
 
     private void OnCollisionEnter(Collision collision)
     {
+        //달릴 때
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Stop();
         }
+        //장애물에 맞았을 때
         else if(collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
         }
     }
 }
