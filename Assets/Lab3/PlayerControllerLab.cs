550b6e1af02e6810c440df8f176d0908bd61a766
hyojoon Lim <hyojoon42@naver.com>
2025-06-15 15:54:53 +0900

    fix : 주석 수정 / 코드 정리

--- Assets/Lab3/PlayerControllerLab.cs
+++ Assets/Lab3/PlayerControllerLab.cs
@@ -1,11 +1,14 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerControllerLab : MonoBehaviour
 {
     public float speed = 10.0f;
+
     private Rigidbody playerRb;
     private float zBound = 4;
 
+    //초기화
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -13,13 +16,24 @@ public class PlayerControllerLab : MonoBehaviour
 
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    //Moves the player based on arrow key input
+    void MovePlayer()
+    {
         //이동
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
 
+    //Prevent the player from leaving the top or bottom of the screen
+    void ConstrainPlayerPosition()
+    {
         if (transform.position.z < -zBound)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
